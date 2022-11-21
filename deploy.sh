7a4076b505a8bd5698587a453fe078a0c99ccdc2
Danny Wen <danny_wen@trendmicro.com>
2022-11-21 14:58:32 +0800

    Change

--- deploy.sh
+++ deploy.sh
@@ -10,13 +10,13 @@ npm run build
 cd dist
 
 # place .nojekyll to bypass Jekyll processing
-echo > .nojekyll
+# echo > .nojekyll
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
 git init
-git checkout -B main
+# git checkout -B main
 git add -A
 git commit -m 'deploy'
 
@@ -24,7 +24,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Furxx2000/Random-quote-ma
-chine-FCC.git main:gh-pages
+git push -f git@github.com:Furxx2000/Random-quote-machine-FCC.git main:gh-pages
 
 cd -
